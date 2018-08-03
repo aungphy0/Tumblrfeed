8704bc55bc3922f1652133775964425ae6738975
aungphyo88 <aphyo@mail.sfsu.edu>
2018-08-02 23:10:44 -0700

    lab 1

---
+++ Tumblrfeed/PhotoCell.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoCell.swift
+//  Tumblrfeed
+//
+//  Created by AUNG PHYO on 8/2/18.
+//  Copyright © 2018 AUNG PHYO. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
